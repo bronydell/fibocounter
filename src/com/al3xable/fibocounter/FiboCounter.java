113c2f9aed2b11f5efd4274dec4d8381f6aeb7d2
Alexander <al3xable@yandex.com>
2016-09-25 13:21:58 +0300

    Added file output and CLI arguments

--- src/com/al3xable/fibocounter/FiboCounter.java
+++ src/com/al3xable/fibocounter/FiboCounter.java
@@ -1,29 +1,53 @@
 package com.al3xable.fibocounter;
 
-import java.io.File;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.math.BigInteger;
 
 public class FiboCounter {
-	File _outputFile = null;
+	String _outputFile = null;
+	BufferedWriter _writer = null;
 	
-	public FiboCounter(File outputFile) {
+	public FiboCounter(String outputFile) {
 		_outputFile = outputFile;
+		
+		try {
+			if (_outputFile != null) {
+				_writer = new BufferedWriter(new FileWriter(outputFile));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
-	private void _out(String str) {
-		System.out.println(str);
+	private void _out(String number, int id) {
+		if (_writer != null) {
+			try {
+				_writer.write(number + '\n');
+				_writer.flush();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			System.out.println(id);
+		} else {
+			System.out.println(number);
+		}
 	}
 	
 	public void start() {
 		BigInteger pre = new BigInteger("0");
 		BigInteger cur = new BigInteger("1");
 		BigInteger sum = new BigInteger("1");
+		int id = 1;
 		
 		while (true) {
-			_out(sum.toString());
+			_out(sum.toString(), id);
 			sum = pre.add(cur);
 			pre = cur;
 			cur = sum;
+			id++;
 		}
 	}
 }
--- src/com/al3xable/fibocounter/Main.java
+++ src/com/al3xable/fibocounter/Main.java
@@ -21,8 +21,39 @@
 
 package com.al3xable.fibocounter;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 public class Main {
 	public static void main(String[] args) {
-		new FiboCounter(null).start();
+
+        Options options = new Options();
+
+        Option output = new Option("o", "output", true, "output file path");
+        output.setRequired(false);
+        options.addOption(output);
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd;
+
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp("utility-name", options);
+
+            System.exit(1);
+            return;
+        }
+
+        String outputFile = cmd.getOptionValue("output"); 
+
+		new FiboCounter(outputFile).start();
 	}
 }
