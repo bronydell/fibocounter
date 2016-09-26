7b88031d370f8d293da8df6b3bbc49ae53fa61c0
Rostislav <littleponyapps@gmail.com>
2016-09-26 16:41:17 +0300

    Added resume mode

--- src/com/al3xable/fibocounter/FiboCounter.java
+++ src/com/al3xable/fibocounter/FiboCounter.java
@@ -1,65 +1,91 @@
 package com.al3xable.fibocounter;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.math.BigInteger;
 
 public class FiboCounter {
-	private String _outputFile = null;
-	private BufferedWriter _writer = null;
-	
-	private BigInteger _pre = new BigInteger("0");
-	private BigInteger _cur = new BigInteger("1");
-	private int _line = 1;
-	
-	public FiboCounter(String outputFile) {
-		_outputFile = outputFile;
-		
-		try {
-			if (_outputFile != null) {
-				_writer = new BufferedWriter(new FileWriter(outputFile));
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	private void _out(String number, int line) {
-		if (_writer != null) {
-			try {
-				_writer.write(number + '\n');
-				_writer.flush();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			
-			System.out.println(line);
-		} else {
-			System.out.println(number);
-		}
-	}
-	
-	public void start() {
-		if (_line == 1) {
-			_out("1", 1);
-		}
-		
-		BigInteger sum;
-		
-		while (true) {
-			_line++;
-			sum = _pre.add(_cur);
-			_pre = _cur;
-			_cur = sum;
-			_out(sum.toString(), _line);
-			
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
+    private String _outputFile = null;
+    private BufferedWriter _writer = null;
+
+    private BigInteger _pre = new BigInteger("0");
+    private BigInteger _cur = new BigInteger("1");
+    private int _line = 1;
+
+    public FiboCounter(String outputFile) {
+        try {
+            ReadLastState(outputFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        _outputFile = outputFile;
+
+        try {
+            if (_outputFile != null) {
+                _writer = new BufferedWriter(new FileWriter(outputFile));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void ReadLastState(String fileName) throws IOException {
+
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+
+        String endLine = null;
+        String preEndLine = null;
+        int lineNumber = 0;
+        String sCurrentLine = null;
+        while ((sCurrentLine = br.readLine()) != null) {
+            preEndLine = endLine;
+            endLine = sCurrentLine;
+            lineNumber++;
+        }
+        if (preEndLine != null && endLine != null) {
+            this._line = lineNumber;
+            System.out.println("Line number: " + lineNumber);
+            this._pre = new BigInteger(preEndLine);
+            System.out.println("Pre-end number: " + preEndLine);
+            this._cur = new BigInteger(endLine);
+            System.out.println("End number: " + endLine);
+        }
+    }
+
+    private void _out(String number, int line) {
+        if (_writer != null) {
+            try {
+                _writer.write(number + '\n');
+                _writer.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println(line);
+        } else {
+            System.out.println(number);
+        }
+    }
+
+    public void start() {
+        if (_line == 1) {
+            _out("1", 1);
+        }
+
+        BigInteger sum;
+
+        while (true) {
+            _line++;
+            sum = _pre.add(_cur);
+            _pre = _cur;
+            _cur = sum;
+            _out(sum.toString(), _line);
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- src/com/al3xable/fibocounter/Main.java
+++ src/com/al3xable/fibocounter/Main.java
@@ -1,36 +1,30 @@
 /**
  * Fibonacci numbers counter
- * 
+ *
  * @copyright Copyright (C) 2016 al3xable <al3xable@yandex.com>. All rights reserved.
  * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License version 3
  * @version 1.0
- *
+ * <p>
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * <p>
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
 
 package com.al3xable.fibocounter;
 
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.*;
 
 public class Main {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
         Options options = new Options();
 
@@ -52,8 +46,9 @@ public class Main {
             return;
         }
 
-        String outputFile = cmd.getOptionValue("output"); 
-
-		new FiboCounter(outputFile).start();
-	}
+        String outputFile = cmd.getOptionValue("output");
+        if (outputFile == null)
+            outputFile = "out.dat";
+        new FiboCounter(outputFile).start();
+    }
 }
